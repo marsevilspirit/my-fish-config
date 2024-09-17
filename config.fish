eba3effb2f4a0b9a380cd6f719fc8402887d862e
marsevilspirit <marsevilspirit@gmail.com>
2024-09-18 02:46:31 +0800

    add fish_right_prompt and fix fish_variables

--- config.fish
+++ config.fish
@@ -1,4 +1,3 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    functions -e fish_complete
 end
---
+++ functions/fish_right_prompt.fish
@@ -0,0 +1,3 @@
+function fish_right_prompt -d "Write out the right prompt"
+    echo "["(date "+%H:%M:%S")"]"
+end
