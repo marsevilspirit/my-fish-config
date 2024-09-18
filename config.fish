0d2b2de8586af29edfc70b642290d26525666d0b
marsevilspirit <marsevilspirit@gmail.com>
2024-09-19 01:24:21 +0800

    取消自动补全

--- config.fish
+++ config.fish
@@ -1,3 +1,9 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+
+    # 取消自动补全
+    set -g fish_autosuggestion_enabled 0
+
+    # leetcode 每日提醒
+    python /home/mars/utils/leetcode_notifyV2.py &
 end
