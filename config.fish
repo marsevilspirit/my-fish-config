beed40bc183137b752c80077947fbf45e2b5a1fb
marsevilspirit <marsevilspirit@gmail.com>
2024-09-21 01:41:22 +0800

    add something

--- config.fish
+++ config.fish
@@ -7,6 +7,23 @@ if status is-interactive
     # neofetch快捷键
     abbr s neofetch
 
+    # kitty快捷键
+    abbr icat "kitty +kitten icat"
+
+    abbr cd z
+
     # leetcode 每日提醒
     python /home/mars/utils/leetcode_notifyV2.py &
+
+    if type -q exa
+        abbr ls exa
+        abbr ll "exa -l -g --icons"
+        abbr lla "ll -a"
+    end
+
+    # fzf
+    fzf --fish | source
+
+    # zoxide 
+    zoxide init fish | source
 end
