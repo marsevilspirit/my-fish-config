481ceb352a0bb15bbf7d862b3a0a5a26647badd6
marsevilspirit <marsevilspirit@gmail.com>
2024-09-23 22:25:39 +0800

    add something

--- config.fish
+++ config.fish
@@ -1,6 +1,8 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 
+    export export CHEAT_CONFIG_PATH="~/.config/cheat/conf.yml"
+
     # 取消自动补全
     set -g fish_autosuggestion_enabled 0
 
@@ -10,14 +12,24 @@ if status is-interactive
     # kitty快捷键
     abbr icat "kitty +kitten icat"
 
+    # zoxide
     abbr cd z
+
+    # bat
+    abbr cat bat
+
+    # duf
+    abbr df duf
+
+    # nvim
+    abbr nv nvim
 
     # leetcode 每日提醒
     python /home/mars/utils/leetcode_notifyV2.py &
 
-    if type -q exa
-        abbr ls exa
-        abbr ll "exa -l -g --icons"
+    if type -q eza
+        abbr ls eza
+        abbr ll "eza -l -g --icons"
         abbr lla "ll -a"
     end
 
